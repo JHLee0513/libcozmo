132c5e9b2a3c5f17ac472d7bf5d883f07bff1224
Brian Lee <JoonHoHere@gmail.com>
2020-03-25 22:19:27 -0700

    configuring torch and dart issues

--- src/model/TorchModel.cpp
+++ src/model/TorchModel.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2019,  Brian Lee, Vinitha Ranganeni
+// Copyright (c) 2019,  Vinitha Ranganeni
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -27,63 +27,50 @@
 // POSSIBILITY OF SUCH DAMAGE.
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef INCLUDE_MODEL_TORCHMODEL_HPP_
-#define INCLUDE_MODEL_TORCHMODEL_HPP_
-
-#include <Eigen/Dense>
-#include <torch/torch.h>
+#include "model/TorchModel.hpp"
+#include <sstream>
+#include <iostream>
+#include "utils/utils.hpp"
 
 namespace libcozmo {
 namespace model {
 
-// Linear regression model in format of PyTorch's C++ API.
-struct LinearRegression : torch::nn::Module {
-  LinearRegression() {
-    fc = register_module("fc", torch::nn::Linear(3, 2));
-  }
+TorchModel::TorchModel() : \
+    m_model(std::make_shared<LinearRegression>()) {}
 
-  // Implement the Net's algorithm.
-  torch::Tensor forward(torch::Tensor x) {
-    // Use one of many tensor manipulation functions.
-    x = fc->forward(x);
-    return x;
-  }
-
-  torch::nn::Linear fc{nullptr};
-};
-
-/// LinearRegression model in PyTorch. Input is a 3 dimensional vector and
-/// ouput is a 2 dimensional vector.
-class TorchModel {
- public:
-
-	TorchModel();
-	~TorchModel() {}
-    /// Gets the end state after applying the given action on the input state
-    /// Input vectors vary based on derived class
-    ///
-    /// \param input_action Given action vector
-    /// \param input_state Given state vector
-    /// \param[out] output_state vector
-    /// \return True if prediction successfully calculated; false otherwise;
-    bool predict_state(
+bool TorchModel::predict_state(
         const Eigen::VectorXd& input_action,
         const Eigen::VectorXd& input_state,
-        Eigen::VectorXd* output_state) const;
+        Eigen::VectorXd* output_state) const {
+    if (input_action.size() != 4 || input_state.size() != 3) {
+        return false;
+    }
 
+    // auto X = torch::tensor({input_action[0], input_action[0], input_action[0], input_action[3]}, 
+    //     torch::requires_grad(false).dtype(torch::kFloat32))
+    //     .view({4,1});
 
-	/// Loades weights into a model.
-	/// \param path Absolute path to the location of the weights file (.pt)
-	/// \return True if loading successful; false otherwise;
-    bool load_weights(const std::string path);
+    // auto Y = m_model->forward(X);
 
-  private:
-	const std::shared_ptr<torch::nn::Module> m_model;
+    // double distance = static_cast<double>(Y.data[0]);
+    // double dtheta = static_cast<double>(Y.data[0]);
 
+    // double x = input_state[0] + distance * cos(dtheta);
+    // double y = input_state[1] + distance * sin(dtheta);
+    // double theta = input_state[2] + dtheta;
+    // *output_state = Eigen::Vector3d(x, y, theta);
+    return true;
+}
 
-};
+bool TorchModel::load_weights(const std::string path) {
+    // try {
+    //     torch::load(m_model, path);
+    //     return true;
+    // } catch (const char* msg) {
+    //     std::cerr << msg << "\n";
+    //     return false;
+    // }
+}
 
 }  // namespace model
 }  // namespace libcozmo
-
-#endif  // INCLUDE_MODEL_MODEL_HPP_
